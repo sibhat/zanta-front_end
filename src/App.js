2f21a7af4083b15db42a26bb09e6fc0aef376f95
sibhat <sibhat.temelso@gmail.com>
2019-05-19 17:43:15 -0700

    list folder deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
-import {withRouter} from "react-router-dom";
 import { CssBaseline} from "@material-ui/core";
-
-
 // import TransitionGroup from "react-transition-group/TransitionGroup";
 import Nav from "./features/nav/container";
-import List from "./features/list/container/"; // ES6
 
 class App extends React.Component {
 
@@ -14,11 +10,10 @@ class App extends React.Component {
             <div className={"root"}>
                 <CssBaseline/> {/* reset css for all devices */}
                 <Nav/>
-                <List/>
             </div>
         );
     }
 }
 
 
-export default withRouter(App);
+export default App;
