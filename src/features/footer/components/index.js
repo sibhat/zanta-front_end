83033ee7b29ee11783303e09569f7c81e85b54d2
sibhat <sibhat.temelso@gmail.com>
2019-07-01 17:03:35 -0700

    implement footer components

---
+++ src/features/footer/components/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import Footer from "../presentational";
+
+export default class  extends Component {
+    
+    render() {
+       return <Footer/>
+    }
+};
+
+
